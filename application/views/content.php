4230ce7d91d969271cd49d3916dbfe2da8960fc7
nirav patel <nirav.ce.2008@gmail.com>
2015-04-29 18:09:16 +0530

    UI

--- application/views/content.php
+++ application/views/content.php
@@ -1,6 +1,23 @@
-<?php
-$this->load->view('template/header');
-$this->load->view('template/nav_bar');
-$this->load->view('template/middle');
-$this->load->view('template/footer');
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php $this->load->view('template/head');?>
+	</head>
+	<body class="skin-blue">
+		<div class="wrapper">
+			<header class="main-header">
+				<?php $this->load->view('template/header');?>
+			</header>
+			<aside class="main-sidebar">
+				<?php $this->load->view('template/sidebar');?>
+			</aside>
+			<div class="content-wrapper" style="min-height: 918px;">
+				<?php $this->load->view($this->router->fetch_class()."/".$view);?>
+			<div>
+			<footer class="main-footer">
+				<?php $this->load->view('template/footer');?>
+			</footer>
+		</div>
+	</body>
+		<?php $this->load->view('template/script');?>
+</html>
